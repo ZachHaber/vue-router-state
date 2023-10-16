673e468658b2fe21a42ce056ceb34b79c0a0b018
Zachary Soare <zjhaber@gmail.com>
2023-10-16 10:07:19 -0500

    build: bundle 3.7.0

--- dist/vue-router.js
+++ dist/vue-router.js
@@ -1,5 +1,5 @@
 /*!
-  * vue-router-2-state v3.6.5
+  * vue-router-2-state v3.7.0
   * Zachary Soare
   * @license MIT
   */
@@ -2898,7 +2898,7 @@
 	 * @internal
 	 */
 	VueRouter.install = install;
-	VueRouter.version = '3.6.5';
+	VueRouter.version = '3.7.0';
 	VueRouter.isNavigationFailure = isNavigationFailure;
 	VueRouter.NavigationFailureType = NavigationFailureType;
 	/**
